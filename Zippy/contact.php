a2e88aea885187ad4bc8febd43a800feca194a54
r556a812 <richardaviles81@gmail.com>
2019-06-24 20:36:50 -0500

    Update jquerry source and used jquerry to get phone number. Uploaded files to a hosting site

--- Zippy/contact.php
+++ Zippy/contact.php
@@ -1,1 +1,35 @@
-empty.
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <title>Zippy Beans Coffee Co.</title>
+
+    <link rel="stylesheet" type="text/css" href="Include/styles.css" />
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+</head>
+
+<body class="grad">
+
+    <div id="banner" name="banner">Zippy Beans Coffee Co.
+        <div id="subhead">Totally Organic, Man!</div>
+    </div>
+    <div id="logo" name="logo">
+        <a href="index.html"><img id="logo_img" alt="Zippy Beans Logo" src="Media/logo.png" /></a>
+    </div>
+    <div id="menu" name="menu"><a href="menu.html">Menu</a> | <a href="direct.html">Hours and Directions</a> | <a href="contact.html">Contact Us!</a></div>
+    <div id="cup" name="cup"><img id="cup_img" alt="Zippy Beans Cup" src="Media/cup.png" /></div>
+    <div id="content" name="content">
+        <?php 
+            Print "Form processing coming soon";
+            ?>
+    </div>
+    <div id="bottom" name="bottom">
+        <div id="disclaimer">This is a fictional company. No coffee beans were harmed in the making of this site.</div>
+    </div>
+
+</body>
+
+</html>
