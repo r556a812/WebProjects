09245ae74da7eb528c94b775a0c44a696512e96f
r556a812 <richardaviles81@gmail.com>
2019-07-02 22:57:02 -0500

    Added movement for the snake

--- Snake/snake.js
+++ Snake/snake.js
@@ -15,6 +15,33 @@ var food_x = Math.floor(Math.random() * 17 + 1) * box; //Game board width is 17 
 var food_y = Math.floor(Math.random() * 15 + 3) * box; //Game board height is 15 boxes with offset of 3 because of the title area
 var food = { x: food_x, y: food_y };
 
+//Listen for input to control the snake
+document.addEventListener("keydown", direction);
+var next_x, next_y;
+next_x = box;
+next_y = 0;
+
+function direction(event) {
+
+    if (event.keyCode == 37) { //Left
+        next_x = -box;
+        next_y = 0;
+
+    } else if (event.keyCode == 38) { //Up
+        next_x = 0;
+        next_y = -box;
+
+    } else if (event.keyCode == 39) { //Right
+        next_x = box;
+        next_y = 0;
+
+    } else if (event.keyCode == 40) { //Down
+        next_x = 0;
+        next_y = box;
+
+    }
+}
+
 //Function to draw to the canvas
 function draw() {
     /***  Create the checkered pattern to show the play area
@@ -55,6 +82,23 @@ function draw() {
     ctx.fillStyle = "red";
     ctx.fillRect(food.x, food.y, box, box);
 
+    //Old position
+    snake_x = snake[0].x;
+    snake_y = snake[0].y;
+
+    //Remove the tail
+    snake.pop();
+
+    //Add new head
+    snake_x += next_x;
+    snake_y += next_y;
+    var new_head = {
+        x: snake_x,
+        y: snake_y
+    }
+    snake.unshift(new_head);
+
+
 }
 
-var game = setInterval(draw, 100);
+var game = setInterval(draw, 500);
